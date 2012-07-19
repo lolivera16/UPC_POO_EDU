c33b902375188189bd036234e1578e14b65366bf
Jean Guzman <jean_27_g@hotmail.com>
2012-07-19 00:19:21 -0500

    clase Cliente

--- src/benedictoxvi/pe/data/Cliente.java
+++ src/benedictoxvi/pe/data/Cliente.java
@@ -1,5 +1,55 @@
 package benedictoxvi.pe.data;
 
 public class Cliente {
+	public String nomCliente;
+	public String apePatCliente;
+    public String apeMatCliente;
+    public String emaCliente;
+    public int dniCliente;
+    public int fonCliente;
+    public String fecConCliente;
+    
+	public String getNomCliente() {
+		return nomCliente;
+	}
+	public void setNomCliente(String nomCliente) {
+		this.nomCliente = nomCliente;
+	}
+	public String getApePatCliente() {
+		return apePatCliente;
+	}
+	public void setApePatCliente(String apePatCliente) {
+		this.apePatCliente = apePatCliente;
+	}
+	public String getApeMatCliente() {
+		return apeMatCliente;
+	}
+	public void setApeMatCliente(String apeMatCliente) {
+		this.apeMatCliente = apeMatCliente;
+	}
+	public String getEmaCliente() {
+		return emaCliente;
+	}
+	public void setEmaCliente(String emaCliente) {
+		this.emaCliente = emaCliente;
+	}
+	public int getDniCliente() {
+		return dniCliente;
+	}
+	public void setDniCliente(int dniCliente) {
+		this.dniCliente = dniCliente;
+	}
+	public int getFonCliente() {
+		return fonCliente;
+	}
+	public void setFonCliente(int fonCliente) {
+		this.fonCliente = fonCliente;
+	}
+	public String getFecConCliente() {
+		return fecConCliente;
+	}
+	public void setFecConCliente(String fecConCliente) {
+		this.fecConCliente = fecConCliente;
+	}
 
 }
