ce238a4559942e48a5d1d3ad08017cbac8b83752
lolivera16 <oliver_aries2@hotmail.com>
2012-07-22 19:49:55 -0500

    Grupo Estudio

--- src/benedictoxvi/pe/data/GrupoEstudio.java
+++ src/benedictoxvi/pe/data/GrupoEstudio.java
@@ -2,4 +2,49 @@ package benedictoxvi.pe.data;
 
 public class GrupoEstudio {
 
+	String NomGrupo;
+	String NomAcademia;
+	String NomCurso;
+	String FecInicio;
+	String FecFin;
+	String Estado;
+	
+	public String getNomGrupo() {
+		return NomGrupo;
+	}
+	public void setNomGrupo(String nomGrupo) {
+		NomGrupo = nomGrupo;
+	}
+	public String getNomAcademia() {
+		return NomAcademia;
+	}
+	public void setNomAcademia(String nomAcademia) {
+		NomAcademia = nomAcademia;
+	}
+	public String getNomCurso() {
+		return NomCurso;
+	}
+	public void setNomCurso(String nomCurso) {
+		NomCurso = nomCurso;
+	}
+	public String getFecInicio() {
+		return FecInicio;
+	}
+	public void setFecInicio(String fecInicio) {
+		FecInicio = fecInicio;
+	}
+	public String getFecFin() {
+		return FecFin;
+	}
+	public void setFecFin(String fecFin) {
+		FecFin = fecFin;
+	}
+	public String getEstado() {
+		return Estado;
+	}
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	
+	
 }
